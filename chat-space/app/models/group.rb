1020c46a5272f756165cdfa9262c60a531874db9
福森賢 <kenkenken0328@icloud.com>
2018-08-12 00:41:03 +0900

    Edit view

--- chat-space/app/models/group.rb
+++ chat-space/app/models/group.rb
@@ -1,6 +1,8 @@
 class Group < ApplicationRecord
   has_many :members
-  has_many :users, through: :members
-  validates :name, presence: true
   has_many :messages
+  has_many :users, through: :members
+
+  validates :name, presence: true
+
 end
