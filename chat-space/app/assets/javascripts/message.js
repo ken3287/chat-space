eb7297f2598fa2a6ee421ea314120146717c9a2d
福森賢 <kenkenken0328@icloud.com>
2018-08-24 23:21:06 +0900

    ビューの変更 jsonの記述　非同期通信の機能作成

--- chat-space/app/assets/javascripts/message.js
+++ chat-space/app/assets/javascripts/message.js
@@ -1,7 +1,46 @@
-$(function(){
-  $('#new_message').on('submit', function(e){
+$(function() {
+  function buildHTML(message) {
+    var img = message.image ? `<img src= ${message.image}>` : "";
+    var html = `<div class="right__message__messages">
+                  <span class="right__message__messages-name">
+                    ${message.user_name}
+                  </span>
+                  <span class="right__message__messages-time">
+                    ${message.created_at}
+                  </span>
+                  <div class="right__message__messages-message">
+                    <p class="right__message__messages-message-body">
+                      ${message.body}
+                    <p/>
+                  <div class="right__message__messages-message__image">
+                    ${img}
+                  </div>
+                  </div>
+                </div>`
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e) {
     e.preventDefault();
-    console.log(this)
     var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.right__message__messages').append(html)
+      $('#new_message')[0].reset();
+      $('.right__message__messages').animate({
+        scrollBottom: $('.right__message__messages')[0].scrollHeight}, 'fast');
+      })
+    .fail(function() {
+      alert('');
+    })
   })
-})
+});
---
+++ chat-space/app/views/messages/create.json.jbuilder
@@ -0,0 +1,5 @@
+json.body @message.body
+json.user_name @message.user.name
+json.image @message.image.url
+json.user_id @message.user.id
+json.created_at @message.created_at
