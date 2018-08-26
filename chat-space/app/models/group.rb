33e1eec3ed862364ec1cbc24f984dde72d437b72
福森賢 <kenkenken0328@icloud.com>
2018-08-26 21:42:52 +0900

    Left contentの編集
    最新のメッセージを表示できるようにした

--- chat-space/app/models/group.rb
+++ chat-space/app/models/group.rb
@@ -1,4 +1,16 @@
 class Group < ApplicationRecord
+
+def show_last_message
+  if (last_message = messages.last).present?
+      if last_message.body?
+      last_message.body
+      else
+      '画像が投稿されています'
+      end
+  else
+  'まだメッセージはありません。'
+  end
+end
   has_many :members
   has_many :messages
   has_many :users, through: :members
