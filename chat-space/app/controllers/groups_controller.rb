4f0fd7a0f3a1cbb2066f4a9e6bc80e3ef3651705
福森賢 <kenkenken0328@icloud.com>
2018-08-27 21:10:59 +0900

    指摘点の修正

--- chat-space/app/controllers/groups_controller.rb
+++ chat-space/app/controllers/groups_controller.rb
@@ -17,10 +17,10 @@ class GroupsController < ApplicationController
     end
   end
 
+
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      @group.users << current_user
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
       render :edit
