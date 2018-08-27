d7a963ace2d82f6d22dc614525eb04680ba30e9c
福森賢 <kenkenken0328@icloud.com>
2018-08-27 14:55:28 +0900

    イベント時の非同期通信
    Routingが設定されていなかったので修正した

--- chat-space/app/assets/javascripts/users.js
+++ chat-space/app/assets/javascripts/users.js
@@ -1,6 +1,13 @@
 $(function() {
-  $("#user-search-field.chat-group-form__input").on("keyup", function() {
-    var input = $("#user-search-field.chat-group-form__input").val();
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-fields").val();
     console.log(input)
+    if((input != "") && (input != " "));
+        $.ajax({
+          type: 'GET',
+          url: '/users',
+          data: { keyword: input },
+          dataType: 'json'
+        })
   });
 });
